f7c700f263e66c74c13884e859fd3e6933fcadf6
3mero <123251092+3mero@users.noreply.github.com>
2025-05-18 20:48:07 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['api.exchangeratesapi.io'],
     unoptimized: true,
-  },
-  // Configuración para evitar problemas con React 18
-  experimental: {
-    esmExternals: 'loose',
   },
   eslint: {
     ignoreDuringBuilds: true,
